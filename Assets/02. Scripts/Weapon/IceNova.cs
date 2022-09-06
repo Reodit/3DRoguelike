2ec24309f180e875f0141b97ede90536d3c92319
heokiro <87606800+heokiro@users.noreply.github.com>
2022-09-06 17:45:14 +0900

    Update IceNova

--- Assets/02. Scripts/Weapon/IceNova.cs
+++ Assets/02. Scripts/Weapon/IceNova.cs
@@ -4,37 +4,64 @@ using UnityEngine;
 
 public class IceNova : MonoBehaviour
 {
+
     public LayerMask whatIsEnemy;
     public ParticleSystem iceNovaParticle;
     public AudioSource iceNovaAudio;
+    public SkillData[] skillData;
+    [Range(1, 3)]public int skillLevel;
 
-    public float weaponDamage = 10f;
-    public float area = 2.5f;
-    public float cooldown = 5f;
-    public bool isCast = true;
+    public float skillDamage = 10f;
+    public float skillArea = 2.5f;
+    public float skillCooldown = 5f;
+
+    public bool isCooldown = true;
 
     private void Start()
     {
-        StartCoroutine("IceNovaCast");
+
     }
 
-    IEnumerator IceNovaCast()
+    private void Update()
     {
-        while (isCast)
+        IceNovaCooldown();
+    }
+
+    public void IceNovaCast()
+    {
+        if (isCooldown)
         {
+            skillDamage = skillData[skillLevel - 1].SkillDamage;
+            skillArea = skillData[skillLevel - 1].SkillArea;
+            skillCooldown = skillData[skillLevel - 1].SkillCooldown;
             iceNovaParticle.Play();
-            Collider[] colliders = Physics.OverlapSphere(transform.position, area, whatIsEnemy);
+            iceNovaAudio.Play();
+            iceNovaParticle.transform.localScale = new Vector3(skillArea, skillArea, skillArea);
+
+            Collider[] colliders = Physics.OverlapSphere(transform.position, 2.5f * skillArea, whatIsEnemy);
 
             for (int i = 0; i < colliders.Length; i++)
             {
                 WeaponTestEnemy targetEnemy = colliders[i].GetComponent<WeaponTestEnemy>();
 
-                float damage = weaponDamage;
+                float damage = skillDamage;
 
                 targetEnemy.TakeDamage(damage);
             }
-            Debug.Log("Ice Nova");
-           yield return new WaitForSeconds(cooldown);
+            isCooldown = false;
+        }
+    }
+
+    void IceNovaCooldown()
+    {
+        if(!isCooldown)
+        {
+            skillCooldown -= Time.deltaTime;
+            if (skillCooldown <= 0)
+            {
+                isCooldown = true;
+                skillCooldown = skillData[skillLevel - 1].SkillCooldown;
+            }
         }
     }
 
